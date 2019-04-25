0cf3fca00482271bc49a35349447f2b3eb09352b
Alexander <maniacallion@gmail.com>
2019-04-25 12:32:46 -0500

    location stuffs

--- server.js
+++ server.js
@@ -15,22 +15,84 @@ client.on('err', err => console.log(err));
 
 app.use(cors());
 
+/* location stuff */
+
 app.get('/location', (request, response) => {
-  try {
-    findLatLong(request, response);
-  } catch (error) {
-    handleErrors(response);
-  }
+  getLocation(request, response);
 });
 
+function getLocation (request, response) {
+  const locationHandler = {
+    query : request.query.data,
+    chaseHit :(results) => {
+      console.log('got data');
+      response.send(results.rows[0]);
+    },
+    chaseMiss : () => {
+      console.log('no data');
+      Location.fetchLocation(request.query.dat)
+        .then(data => response.send(data));
+    }
+  };
+  Location.lookupLocation(locationHandler);
+}
+
+Location.lookupLocation = (handler => {
+  const SQL = 'SELECT * FROM locations WHERE search_query = $1';
+  const values = [handler.query];
+
+  return client.query (SQL, values)
+    .then(results => {
+      if (results.rowCount > 0) {
+        handler.cacheHit(results);
+      }
+      else {
+        handler.cacheMiss();
+      }
+    }).catch(error => {
+      console.log(error);
+    });
+});
+
+Location.fetchLocation = (query) => {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+  return superagent.get(url)
+    .then (res => {
+      console.log('got stuff');
+      if(!data.body.results.length) {
+        throw 'no data';
+      }
+      else {
+        let location = new Location (query, res.body.results[0]);
+        return location.save().then(results => {
+          location.id = results.rows[0].id;
+          return location;
+        });
+      }
+    });
+};
+
+function Location(query, res) {
+  (this.searchQuery = query),
+  (this.formattedQuery = res.body.results[0].formatted_address),
+  (this.latitude = res.body.results[0].geometry.location.lat),
+  (this.longitude = res.body.results[0].geometry.location.lng);
+}
+Location.prototype.save = function() {
+  const SQL = `
+  INSERT INTO locations
+    (serach_query, formated_query, latitude, longitude)
+    VALUES ($1, $2, $3, $4)
+    RETRURN id`;
+  let values = Object.values(this);
+  return client.query (SQL, values);
+};
+
+
+/*weather stuff*/
+
 app.get('/weather', (request, response) => {
-  // try {
   getWeather(request, response);
-  // }
-  // catch (error) {
-  //   console.log(error);
-  //   handleErrors(response);
-  // }
 });
 
 const getWeather = (request, response) => {
@@ -65,12 +127,7 @@ const findLatLong = (request, response) => {
     });
 };
 
-function Location(query, res) {
-  (this.searchQuery = query),
-  (this.formattedQuery = res.body.results[0].formatted_address),
-  (this.latitude = res.body.results[0].geometry.location.lat),
-  (this.longitude = res.body.results[0].geometry.location.lng);
-}
+
 
 // ERROR HANDLING
 
